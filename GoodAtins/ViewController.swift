d7efa900cc8ece1eabbda659830be3b2a9858fee
abdullahgebreil <abdullahgebreil@gmail.com>
2019-07-16 09:55:54 +0100

    Initial Commit

---
+++ GoodAtins/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoodAtins
+//
+//  Created by Abdullah Gebreil on 11/14/1440 AH.
+//  Copyright © 1440 AH Abdullah Gebreil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
