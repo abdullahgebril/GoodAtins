97d678125872d71d7f234838b93855be8dfba71f
abdullahgebreil <abdullahgebreil@gmail.com>
2019-09-03 00:24:33 +0100

    first commit

---
+++ GoodAtins/Model/DataSet.swift
@@ -0,0 +1,9 @@
+//
+//  DataSet.swift
+//  GoodAtins
+//
+//  Created by Abdullah Gebreil on 11/14/1440 AH.
+//  Copyright © 1440 AH Abdullah Gebreil. All rights reserved.
+//
+
+import Foundation
---
+++ GoodAtins/View/CategoryCell.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryCell.swift
+//  GoodAtins
+//
+//  Created by Abdullah Gebreil on 11/14/1440 AH.
+//  Copyright © 1440 AH Abdullah Gebreil. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
